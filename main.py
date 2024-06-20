d85b7f67aa1c5abf1a32d8c50f96f6618348cfea
Gabriel Duarte <gabriel.anpedu@gmail.com>
2024-06-20 00:32:13 -0300

    criação das funçoes para salvar, excluir e carregar as marcações, criação da marcação das estrelas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from tkinter import simpledialog
+from tkinter import simpledialog, messagebox
 import math
 
 pygame.init()
@@ -13,17 +13,68 @@ icone = pygame.image.load("assets/space.png")
 fonte = pygame.font.SysFont("Arial", 18)
 som = pygame.mixer.Sound("assets/Space_Machine_Power.mp3")
 som.play(-1)
+estrelas = {}
+raio = 5
 branco = (225,225,225)
 preto = (0,0,0)
+
+
+
+def salvarMarcacoes():
+     with open("Estralas Marcadas.txt", "w") as arquivo:
+          for nome, posicao in estrelas.items():
+               arquivo.write(f"{posicao[0]},{posicao[1]},{nome}\n")
+
+def carregarMarcacoes():
+     estrelas.clear()
+     try:
+          with open("Estrelas Marcadas.txt", "r") as arquivo:
+               for linha in arquivo:
+                    try:
+                         x, y, nome = linha.strip().split(",")
+                         posicao = (int(x), int(y))
+                         estrelas[nome] = posicao
+                    except ValueError:
+                         continue
+     except FileNotFoundError:
+          messagebox.showinfo("Erro", "Arquivo não encontrado.")     
+
+
+def excluirMarcacoes():
+     estrelas.clear()
 
 
 while True:
      for evento in pygame.event.get():
           if evento.type == pygame.QUIT:
                quit()
+               salvarMarcacoes()
+          elif evento.type == pygame.KEYDOWN:
+               if evento.key ==pygame.K_ESCAPE:
+                    pygame.quit()
+                    salvarMarcacoes()
+               elif evento.key == pygame.K_F10:
+                    salvarMarcacoes()
+               elif evento.key == pygame.K_F11:
+                    carregarMarcacoes()
+               elif evento.key == pygame.K_F12:
+                    excluirMarcacoes()
+          elif evento.type == pygame.MOUSEBUTTONUP:
+               pos = pygame.mouse.get_pos()
+               item = simpledialog.askstring("Space", "Nome da Estrela: ")
+               print(item)
+               if item == None:
+                    item = "desconhecido"+str(pos)
+               estrelas[item] = pos
 
      tela.fill(branco)
      tela.blit(fundo,(0,0))
+
+     for item, posicoes in estrelas.items():
+        pygame.draw.circle(tela, branco, posicoes, raio)
+        fonte = pygame.font.Font(None, 20)
+        texto = fonte.render(item, True, (255, 255, 255))
+        tela.blit(texto, (posicoes[0] + 10, posicoes[1] + 10))
 
      textoOpcoes = fonte.render("Opções:", True, branco)
      textoSalvar = fonte.render("F10 - Salvar marcações", True, branco)
